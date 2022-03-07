6932a3afe7aed4e375c1533f4b799efb562cacbe
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2022-03-07 15:45:18 +0200

    Created form 03 tests

--- src/forms/course-grade/CourseGradeService.test.ts
+++ src/forms/course-grade/CourseGradeService.test.ts
@@ -6,4 +6,17 @@ test('renders learn react link', () => {
   const {calculate} = CourseGradeService();
 
   // Add some tests (at least 2-3 tests per grade)
+  expect(calculate(24, 30, 30)).toBe('failed');
+  expect(calculate(30, 24, 30)).toBe('failed');
+  expect(calculate(30, 30, 24)).toBe('failed');
+  expect(calculate(25, 25, 25)).toBe('failed');
+
+  expect(calculate(25, 25, 26)).toBe('satisfactory');
+  expect(calculate(25, 25, 50)).toBe('satisfactory');
+
+  expect(calculate(40, 30, 31)).toBe('good');
+  expect(calculate(50, 50, 25)).toBe('good');
+
+  expect(calculate(50, 50, 26)).toBe('very good');
+  expect(calculate(50, 50, 50)).toBe('very good');
 });
